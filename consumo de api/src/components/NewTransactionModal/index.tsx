4c59225b22e7362dbfa5958fde75829dc501957a
Caio <caiocpe@gmail.com>
2024-09-22 19:05:15 -0300

    funcionalidade de filtragem feita com 'fetch' nativo

--- consumo de api/src/components/NewTransactionModal/index.tsx
+++ consumo de api/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,42 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay, TransactionTypeButton, TrasactionType } from './styles'
+import {
+    CloseButton,
+    Content,
+    Overlay,
+    TransactionTypeButton,
+    TrasactionType,
+} from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome']),
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { isSubmitting },
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema),
+        defaultValues: {
+            type: 'income',
+        },
+    })
+
+    function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        console.log(data)
+    }
+
     return (
         <>
             <Dialog.Portal>
@@ -12,22 +46,56 @@ export function NewTransactionModal() {
                     <CloseButton>
                         <X size={24} />
                     </CloseButton>
-                    <form action="">
-                        <input type="text" placeholder="Descrição" required />
-                        <input type="number" placeholder="Preço" required />
-                        <input type="text" placeholder="Categoria" required />
+                    <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                        <input
+                            {...register('description')}
+                            type="text"
+                            placeholder="Descrição"
+                            required
+                        />
+                        <input
+                            {...register('price', { valueAsNumber: true })}
+                            type="number"
+                            placeholder="Preço"
+                            required
+                        />
+                        <input
+                            {...register('category')}
+                            type="text"
+                            placeholder="Categoria"
+                            required
+                        />
 
-                        <TrasactionType>
-                            <TransactionTypeButton variant='income' value='income'>
-                                <ArrowCircleUp size={24} />
-                                Entrada
-                            </TransactionTypeButton>
-                            <TransactionTypeButton variant='outcome' value='outcome'>
-                                <ArrowCircleDown size={24} />
-                                Saída
-                            </TransactionTypeButton>
-                        </TrasactionType>
-                        <button type="submit">Cadastrar</button>
+                        <Controller
+                            control={control}
+                            name="type"
+                            render={({ field }) => {
+                                return (
+                                    <TrasactionType
+                                        onValueChange={field.onChange}
+                                        value={field.value}
+                                    >
+                                        <TransactionTypeButton
+                                            variant="income"
+                                            value="income"
+                                        >
+                                            <ArrowCircleUp size={24} />
+                                            Entrada
+                                        </TransactionTypeButton>
+                                        <TransactionTypeButton
+                                            variant="outcome"
+                                            value="outcome"
+                                        >
+                                            <ArrowCircleDown size={24} />
+                                            Saída
+                                        </TransactionTypeButton>
+                                    </TrasactionType>
+                                )
+                            }}
+                        />
+                        <button type="submit" disabled={isSubmitting}>
+                            Cadastrar
+                        </button>
                     </form>
                 </Content>
             </Dialog.Portal>
--- consumo de api/src/pages/Transactions/components/SearchForm/index.tsx
+++ consumo de api/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,37 @@
-import { MagnifyingGlass } from "phosphor-react";
-import { SearchhFormContainer } from "./style";
+import { MagnifyingGlass } from 'phosphor-react'
+import { SearchhFormContainer } from './style'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContext } from 'react'
 
-export function SearchForm(){
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function SearchForm() {
+    const { register, handleSubmit, formState:{isSubmitting} } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema),
+    })
+    const {fetchTransactions} = useContext(TransactionsContext)
+
+    async function handleSearchTransactions(data: SearchFormInputs){
+        await fetchTransactions(data.query)
+    }
+
     return (
-        <SearchhFormContainer>
-            <input type="text" name="" id="" placeholder="Busque por transações" />
-            <button type="submit">Buscar <MagnifyingGlass size={20} /></button>
+        <SearchhFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                {...register('query')}
+            />
+            <button type="submit" disabled={isSubmitting}>
+                Buscar <MagnifyingGlass size={20} />
+            </button>
         </SearchhFormContainer>
     )
 }
