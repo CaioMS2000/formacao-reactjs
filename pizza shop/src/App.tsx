975bc0f2548d4ce20b2adf168c43e92c94402e10
Caio <caiocpe@gmail.com>
2024-09-23 02:09:57 -0300

    configurações iniciais

---
+++ pizza shop/src/App.tsx
@@ -0,0 +1,16 @@
+import { RouterProvider } from "react-router-dom";
+import "./index.css"
+import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+export function App() {
+
+  return (
+    <>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+    </>
+  )
+}
